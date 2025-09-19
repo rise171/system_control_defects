7c51a5e1e79c2be98a1e4e5239d41dac1bd8f1e2
rise171 <krispetrova206@gmail.com>
2025-09-19 23:00:31 +0300

    update vore settings and database settings

---
+++ backend/app/core/security.py
@@ -0,0 +1,53 @@
+from fastapi import Depends
+from jose import JWTError
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models import User
+from settings.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from settings.database import get_session
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_jwt_token(data: dict, expires_delta: timedelta = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_jwt_token(token: str) -> dict | None:
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.PyJWTError:
+        return None
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from jose import JWTError, jwt
+from typing import Optional
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# Зависимость для получения текущего пользователя из токена
+def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_session)) -> int:
+    try:
+        # Декодируем токен и извлекаем данные
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return user_id
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
---
+++ backend/app/database/settings.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+engine = create_async_engine("sqlite+aiosqlite:///project.db", echo=True)
+make_session = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_session():
+    async with make_session() as session:
+        yield session
+
+class Base(DeclarativeBase):
+    pass
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def delete_tables():
+    async with engine.begin() as conn:
+        await  conn.run_sync(Base.metadata.drop_all())
