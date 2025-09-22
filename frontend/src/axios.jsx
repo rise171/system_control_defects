67e6f47e14771d62a31bbf4439fbbf3367858797
rise171 <krispetrova206@gmail.com>
2025-09-22 22:41:34 +0300

    basic frontend setup

---
+++ frontend/src/axios.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+// Создаем экземпляр axios с базовым URL
+export const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Добавляем перехватчик для обработки ошибок
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('authToken');
+      window.location.reload();
+    }
+    return Promise.reject(error);
+  }
+);
